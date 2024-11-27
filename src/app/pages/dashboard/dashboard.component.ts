a4d16f4448a6c40551dc374be28884b0fae12f08
pamp24 <pamporakis.fotis@gmail.com>
2024-11-27 19:31:25 +0200

    DashboardPart2

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -13,7 +13,7 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.less']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
   // General overview data
   totalPaid = 129.56;
   nextDueDate = new Date(2024, 6, 15);
@@ -21,94 +21,286 @@ export class DashboardComponent {
   // Expenses
   expenses = [
     { category: 'Συνεργείο Καθαρισμού', amount: 150.0 },
-    { category: 'Ρεύμα', amount: 240.0 },
-    { category: 'Αμοιβή Γραφείου', amount: 120.0 },
-    { category: 'Απεντόμωση', amount: 110.0 }
+    { category: 'Ρεύμα', amount: 200.0 },
+    { category: 'Αμοιβή Γραφείου', amount: 100.0 },
+    { category: 'Απεντόμωση', amount: 150.0 }
   ];
   summarizeExpsenses() {
     return this.expenses.reduce((total, expenses) => total + expenses.amount, 0);
   }
-  //elevator
-  elevatorExpenses = [
-    { category: 'Συντήρηση', amount: 150.0 },
-  ];
-  summarizeEleExpsenses() {
-    return this.elevatorExpenses.reduce((total, elevatorExpenses) => total + elevatorExpenses.amount, 0);
-  }
-  //heating
+
+  //Heating
   heatingExpenses = [
     { category: 'Πετρέλαιο', amount: 200.00 },
   ];
   summarizeHeatingExpsenses() {
     return this.heatingExpenses.reduce((total, heatingExpenses) => total + heatingExpenses.amount, 0);
   }
-  
-  residents = [
+  //Elevator
+  elevatorExpenses = [
+    { category: 'Συντήρηση', amount: 200 },
+  ];
+  summarizeEleExpsenses() {
+    return this.elevatorExpenses.reduce((total, elevatorExpenses) => total + elevatorExpenses.amount, 0);
+  }
+  //Autonomy
+  autonomyExpenses = [
+    { category: 'Αυτονομία', amount: 200 },
+  ];
+  summarizeAutonomyExpsenses() {
+    return this.autonomyExpenses.reduce((total, autonomyExpenses) => total + autonomyExpenses.amount, 0);
+  }
+  //Boiler
+  boilerExpenses = [
+    { category: 'Boiler', amount: 100 },
+  ];
+  summarizeBoilerExpsenses() {
+    return this.boilerExpenses.reduce((total, boilerExpenses) => total + boilerExpenses.amount, 0);
+  }
+  //Special Expenses
+  specialExpenses = [
+    { category: 'ΔΕΗ Υπηρεσίας', amount: 30 },
+  ];
+  summarizeSpecialExpsenses() {
+  return this.specialExpenses.reduce((total, specialExpenses) => total + specialExpenses.amount, 0);
+  }
+  //Owner Expenses
+  ownersExpenses = [
+    { category: 'Συναγερμός', amount: 15 },
+  ];
+  summarizeOwnersExpsenses() {
+   return this.ownersExpenses.reduce((total, ownersExpenses) => total + ownersExpenses.amount, 0);
+  }
+  //Equal Expenses
+  equalExpenses = [
+    { category: 'Σκύλος', amount: 30 },
+  ];
+  summarizeEqualExpsenses() {
+   return this.equalExpenses.reduce((total, equalExpenses) => total + equalExpenses.amount, 0);
+  }
+  //Data
+  residents: any[] = [
     {
       id: '001',
       name: 'ΛΑΙΜΟΣ',
       apartment: 'Υ1',
-      appartmentInstance: 10,
-      elevator: 0,
-      heating: 12.71,
-      individual: 10.00,
-      boiler: 12.71,
-      common: 5.00,
-      elevatorExpense: 4.00,
-      heatingExpense: 2.50,
-      individualExpense: 2.00,
-      boilerExpense: 1.50,
-      closed: 0.00,
+      appartmentInstance: 20,
+      elevatorInstance: 0,
+      heatingInstance: 60,
+      individual: 0,
+      boiler: 100,
+      common: 0,
+      elevatorExpense: 0,
+      heatingExpense: 0,
+      individualExpense: 0,
+      boilerExpense: 0,
+      closed: 0,
       special:0,
-      owners: 2.00,
-      equalParts: 3.00,
-      issuance: 1.00,
-      autonomous: 1.50,
-      rounded: 0.50,
-      total: 20.50
+      owners: 0,
+      equalParts: 0,
+      issuance: 0,
+      autonomous: 0,
+      rounded: 0,
+      total: 0
     },
     {
       id: '002',
       name: 'ΠΑΠΑΝΔΡΕΟΥ',
       apartment: 'O1',
       appartmentInstance: 50,
-      elevator: 16.91,
-      heating: 12.59,
-      individual: 9.50,
-      boiler: 12.59,
-      common: 4.50,
-      elevatorExpense: 3.50,
-      heatingExpense: 2.00,
-      individualExpense: 1.80,
-      boilerExpense: 1.20,
+      elevatorInstance: 50,
+      heatingInstance: 10,
+      individual: 0,
+      boiler: 0,
+      common: 0,
+      elevatorExpense: 0,
+      heatingExpense: 0,
+      individualExpense: 0,
+      boilerExpense: 0,
       closed: 0.00,
       special:0,
-      owners: 1.50,
-      equalParts: 2.50,
-      issuance: 1.20,
-      autonomous: 1.00,
-      rounded: 0.40,
-      total: 20.30
+      owners: 0,
+      equalParts: 0,
+      issuance: 0,
+      autonomous: 0,
+      rounded: 0,
+      total: 0
     },
+    {
+      id: '003',
+      name: 'ΝΙΚΟΛΑΟΥ',
+      apartment: 'O2',
+      appartmentInstance: 30,
+      elevatorInstance: 50,
+      heatingInstance: 30,
+      individual: 100,
+      boiler: 0,
+      common: 0,
+      elevatorExpense: 0,
+      heatingExpense: 0,
+      individualExpense: 0,
+      boilerExpense: 0,
+      closed: 0,
+      special: 0,
+      owners: 0,
+      equalParts: 0,
+      issuance: 0,
+      autonomous: 0,
+      rounded: 0,
+      total: 0
+    }
     
   ];
-  // Announcements
+  //Announcements
   announcements = [
     { date: '15/07/2024', message: 'Μηνιαία Συνάντηση 17:00-18:00.' },
     { date: '10/07/2024', message: 'Καθαρισμός κοινόχρηστων χώρων.' }
   ];
-  totalSum(){
+
+  totalSumOfExpenses(){
     return this.summarizeExpsenses() + 
     this.summarizeEleExpsenses() +
-    this.summarizeHeatingExpsenses();
+    this.summarizeHeatingExpsenses() +
+    this.summarizeAutonomyExpsenses() +
+    this.summarizeBoilerExpsenses() +
+    this.summarizeSpecialExpsenses() +
+    this.summarizeEqualExpsenses();
   }
-  totalSumPerResident(){
-    for (let i=0; i<=this.residents.length; i++){
-      
+
+  totalsPerResident(residents: number[],){
+    const length = residents.length;
+    for (let i=0;i<residents.length;i++){
+      if (!residents[i]) continue;
+      const totalExpeSumPerCustomer = this.summarizeExpsenses()/100 * this.residents[i].appartmentInstance;
+      const totalElevSumPerCustomer = (this.summarizeEleExpsenses()/100) * this.residents[i].elevatorInstance;
+      const totalHeatSumPerCustomer = (this.summarizeHeatingExpsenses()/100) * this.residents[i].heatingInstance;
+      const totalAutonomySumPerCustomer = (this.residents[i].individual/this.summarizeAutonomyExpsenses())* this.residents[i].individual;
+      const totalBoilerSumPerCustomer = (this.residents[i].boiler/this.summarizeBoilerExpsenses())* this.residents[i].boiler;
+      const totalSpecialSumPerCustomer = this.summarizeSpecialExpsenses()/length;
+      const totalOwnersSumPerCustomer = this.summarizeOwnersExpsenses()/length;
+      const totalEqualSumPerCustomer = this.summarizeEqualExpsenses()/length;
+
+      this.residents[i].common = totalExpeSumPerCustomer; 
+      this.residents[i].elevatorExpense = totalElevSumPerCustomer;
+      this.residents[i].heatingExpense = totalHeatSumPerCustomer;
+      this.residents[i].individualExpense = totalAutonomySumPerCustomer;
+      this.residents[i].boilerExpense = totalBoilerSumPerCustomer;
+      this.residents[i].special = totalSpecialSumPerCustomer;
+      this.residents[i].owner = totalOwnersSumPerCustomer;
+      this.residents[i].equalParts = totalEqualSumPerCustomer;
+
+    };
+    return this.residents;
+  }
+
+  //Standart Values
+  totalMilimitersMilimiters(residents: any[]): number{
+    let totalMilimitersSum = 0;
+    for(let i= 0; i<this.residents.length; i++){
+      if (!residents[i]) continue;
+      totalMilimitersSum += this.residents[i]?.appartmentInstance;
     }
+    return totalMilimitersSum;
   }
+  totalElevatorMilimiters(residents: any[]): number{
+    let totalElevatorSum = 0;
+    for(let i= 0; i<this.residents.length; i++){
+      if (!residents[i]) continue;
+      totalElevatorSum += this.residents[i]?.elevatorInstance;
+    }
+    return totalElevatorSum;
+  }
+  totalHeatMilimiters(residents: any[]): number{
+    let totalHeatSum = 0;
+    for(let i= 0; i<this.residents.length; i++){
+      if (!residents[i]) continue;
+      totalHeatSum += this.residents[i]?.heatingInstance;
+    }
+    return totalHeatSum;
+  }
+  //koinoxrista se lefta
+  //koinoxrista
+  totalSumExpenses(residents: any[]){
+    let totalExpensesSum = 0;
+    for(let i= 0; i<this.residents.length; i++){
+      if (!residents[i]) continue;
+      totalExpensesSum += this.residents[i]?.common;
+    }
+    return totalExpensesSum;
+  }
+  //asanser
+  totalElevExpenses(residents: any[]){
+    let totalElevatorSum = 0;
+    for(let i= 0; i<this.residents.length; i++){
+      if (!residents[i]) continue;
+      totalElevatorSum += this.residents[i]?.elevatorExpense;
+    }
+    return totalElevatorSum;
+  }
+  //thermansi
+  totalHeatExpenses(residents: any[]){
+    let totalHeatSum = 0;
+    for(let i= 0; i<this.residents.length; i++){
+      if (!residents[i]) continue;
+      totalHeatSum += this.residents[i]?.heatingExpense;
+    }
+    return totalHeatSum;
+  }
+  //atomiki
+  totalAutonomyExpenses(residents: any[]){
+    let totalAutonomySum = 0;
+       for(let i= 0; i<this.residents.length; i++){
+       if (!residents[i]) continue;
+        totalAutonomySum += this.residents[i]?.individualExpense;
+    }
+    return totalAutonomySum;
+  }
+  //Boiler
+  totalBoilerExpenses(residents: any[]){
+    let totalBoilerSum = 0;
+       for(let i= 0; i<this.residents.length; i++){
+       if (!residents[i]) continue;
+        totalBoilerSum += this.residents[i]?.boilerExpense;
+    }
+    return totalBoilerSum;
+  }
+  //isa meri
+  totalEqualExpenses(residents: any[]){
+    let totalEqualSum = 0;
+      for(let i= 0; i<this.residents.length; i++){
+        if (!residents[i]) continue;
+        totalEqualSum += this.residents[i]?.equalParts;
+      }
+      return totalEqualSum;
+  }
+  //Idioktites
+  totalOwnersExpenses(residents: any[]){
+    let totalOwnersSum = 0;
+      for(let i= 0; i<this.residents.length; i++){
+        if (!residents[i]) continue;
+        totalOwnersSum += this.residents[i]?.owners;
+      }
+      return totalOwnersSum;
+  }  
+  //Full Sum Per Customer
+  fullSumPerCustomer(fullSum: number){
+    for(let i = 0; i<this.residents.length; i++){
+      fullSum = this.totalSumExpenses(this.residents[i])+
+        this.totalElevExpenses([i])+
+        this.totalHeatExpenses([i])+
+        this.totalAutonomyExpenses([i])+
+        this.totalBoilerExpenses([i])+
+        this.totalEqualExpenses([i]);
+    }
+    return fullSum;
+  }
+  //Rest of Functions
   payNow() {
     alert('Redirecting to payment page...');
+  }
+
+  ngOnInit(): void {
+    this.totalsPerResident(this.residents); 
   }
 }
 
